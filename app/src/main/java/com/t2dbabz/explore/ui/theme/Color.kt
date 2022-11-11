0b53de58cd1e4a491035ddea0b4aff77f7d3f191
Babatunde Owoleke <tundeowoleke1@gmail.com>
2022-11-11 04:44:43 +0100

    Add app colors

--- app/src/main/java/com/t2dbabz/explore/ui/theme/Color.kt
+++ app/src/main/java/com/t2dbabz/explore/ui/theme/Color.kt
@@ -6,3 +6,23 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val GraySearch = Color(0xFFF2F4F7)
+val GraySearchText = Color(0xFF667085)
+
+val FilterBorder = Color(0xFFA9B8D4)
+
+val DarkBackground = Color(0xFF000F24)
+
+
+val LightTextGray = Color(0xFF98A2B3)
+val LightTextBlack = Color(0xFF1C1917)
+
+
+val DarkTextBlack = Color(0xFFF2F4F7)
+val DarkTextGray = Color(0xFF98A2B3)
+
+val DarkIconTint = Color(0xFFD0D5DD)
+val LightIconTint = Color(0xFF1C1917)
+
+val FilterButton = Color(0xFFFF6C00)
